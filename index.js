c9489660a5f51fb80e9eec2a9de1604973409cfa
Richie Carmichael <richie.carmichael@gmail.com>
2016-06-17 10:12:19 -0700

    Updates

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ require({
     'esri/layers/ImageParameters',
     'esri/symbols/PictureMarkerSymbol',
     'esri/symbols/SimpleLineSymbol',
+    'esri/renderers/SimpleRenderer',
     'esri/tasks/query',
     'dojo/domReady!'
 ],
@@ -50,10 +51,13 @@ function (
     ImageParameters,
     PictureMarkerSymbol,
     SimpleLineSymbol,
+    SimpleRenderer,
     Query
     ) {
     // Enforce strict mode
     'use strict';
+
+    // http://vanseodesign.com/web-design/svg-markers/
 
     // Constants
     var LOS_ANGELES = new Point(-2296480, 843170, new SpatialReference(102010));
@@ -61,6 +65,8 @@ function (
     var BASEMAP = 'http://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer';
     var SOUTH = { "type": "polyline", "paths": [[[2938539, 1263738], [2858729, 1171455], [2758186, 1093395], [2028232, 418784], [1822923, 185238], [1765819, 62535], [1738032, -70050], [1743821, -155229], [1780317, -282973], [1775639, -374501], [1734214, -453757], [1542248, -700489], [1505450, -766678], [1488932, -889779], [1501360, -1017839], [1564493, -1271011], [1594511, -1492129], [1547396, -1595103], [1385292, -1694741], [1220855, -1767090], [1128644, -1869224], [1104448, -1964691], [1136300, -2118935], [1576806, -2600735], [1662836, -2732655], [1734642, -2875301], [1883989, -3242951], [1931338, -3269459], [1962864, -3363313], [1961301, -3449569], [1901384, -3516255], [1780303, -3535213], [1649302, -3537437], [1282518, -3430011], [-74925, -2817204], [-660442, -2582150], [-959207, -2424283], [-1266084, -2156221], [-1924285, -1275918], [-2040016, -1084223], [-2157877, -790186], [-2251542, -428473], [-2334951, 194682], [-2343778, 462530], [-2292667, 810791], [-2198273, 1161879], [-2195456, 1333027], [-2209754, 1556351], [-2290912, 1853623], [-2348512, 2163300], [-2444200, 2431399], [-2568829, 2645063], [-2761371, 2797498], [-2904620, 2869677], [-3001580, 2891360], [-3194048, 2878763], [-3481383, 2887330], [-3657672, 2946961], [-3807854, 3023180], [-3889954, 3096419], [-3927213, 3165653], [-3945137, 3248585], [-3944447, 3343159], [-3830175, 3631348], [-3710463, 3773768], [-3584520, 3870685], [-3367896, 3955416], [-3278048, 4036637], [-3237660, 4150151], [-3125408, 4264800], [-2960711, 4396813], [-2801197, 4453920], [-2628171, 4475554], [-2409733, 4413795], [-2235853, 4340509], [-1227996, 3666627], [-927017, 3379124], [-798300, 3271982], [-770331, 3215932], [-739183, 3190859], [-710937, 3187674], [-466679, 3255542], [-395862, 3221227], [-336865, 3207661], [-283840, 3216417], [-173807, 3278609], [-64133, 3452537], [15484, 3551658], [60271, 3564136], [127548, 3547021], [167416, 3518123], [228633, 3428977], [290923, 3373706], [346347, 3346621], [506677, 3362314], [648982, 3341641], [674962, 3317629], [781413, 3134867], [843458, 2857406], [870063, 2770184], [895082, 2750654], [1041610, 2736453], [1374170, 2614276], [1455033, 2607340], [1643914, 2627348], [1711631, 2619579], [1775139, 2593291], [2682125, 2024214], [2917508, 1740819], [3015900, 1558885], [3017429, 1477893], [3002190, 1385416], [2967922, 1297061], [2938539, 1263738]]], "_path": 0, "spatialReference": { "wkid": 102010, "latestWkid": 102010 }, "cache": { "_extent": { "xmin": -3945137, "ymin": -3537437, "xmax": 3017429, "ymax": 4475554, "spatialReference": { "wkid": 102010, "latestWkid": 102010 } }, "_partwise": null } };
     var NORTH = { "type": "polyline", "paths": [[[2873027, 1152057], [2355718, 727931], [2028232, 418784], [1822923, 185238], [1765819, 62535], [1738032, -70050], [1743821, -155229], [1780317, -282973], [1775639, -374501], [1734214, -453757], [1542248, -700489], [1505450, -766678], [1488932, -889779], [1501360, -1017839], [1564493, -1271011], [1594511, -1492129], [1547396, -1595103], [1385292, -1694741], [1220855, -1767090], [1128644, -1869224], [1104448, -1964691], [1136300, -2118935], [1442008, -2444014], [1576806, -2600735], [1659263, -2725557], [1734642, -2875301], [1883989, -3242951], [1931338, -3269459], [1962864, -3363313], [1961301, -3449569], [1901384, -3516255], [1780303, -3535213], [1649302, -3537437], [1282518, -3430011], [-74925, -2817204], [-660442, -2582150], [-959207, -2424283], [-1266084, -2156221], [-1924285, -1275918], [-2040016, -1084223], [-2157877, -790186], [-2251542, -428473], [-2334951, 194682], [-2332841, 463157], [-2278969, 807862], [-2185406, 1155579], [-2179519, 1333027], [-2192745, 1524585], [-2348512, 2163300], [-2444200, 2431399], [-2568829, 2645063], [-2761371, 2797498], [-2904620, 2869677], [-3001580, 2891360], [-3194048, 2878763], [-3481383, 2887330], [-3657672, 2946961], [-3807854, 3023180], [-3889954, 3096419], [-3927213, 3165653], [-3945137, 3248585], [-3944447, 3343159], [-3830175, 3631348], [-3710463, 3773768], [-3584520, 3870685], [-3367896, 3955416], [-3278048, 4036637], [-3237660, 4150151], [-3125408, 4264800], [-2960711, 4396813], [-2801197, 4453920], [-2628171, 4475554], [-994449, 4074929], [-719566, 3871272], [-604143, 3836959], [-419672, 3824824], [-159184, 3835992], [188289, 3824086], [496476, 3836817], [734133, 3860850], [1015504, 3757737], [1285493, 3607028], [1640847, 3329190], [1881724, 3020845], [2889232, 1817187], [2940863, 1750376], [3034866, 1574804], [3050912, 1513154], [3036555, 1378876], [3006997, 1303714], [2988478, 1268368], [2873027, 1152057]]], "_path": 0, "spatialReference": { "wkid": 102010, "latestWkid": 102010 }, "cache": { "_extent": { "xmin": -3945137, "ymin": -3537437, "xmax": 3050912, "ymax": 4475554, "spatialReference": { "wkid": 102010, "latestWkid": 102010 } }, "_partwise": null } };
+    var NORMAL_PASSAGE = new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new Color([50, 50, 50, 0.5]), 10);
+    var SELECTED_PASSAGE = new SimpleLineSymbol(SimpleLineSymbol.STYLE_SHORTDASH, new Color([255, 0, 0, 1]), 10);
 
     // Dragging member
     var _dragOffset = null;   
@@ -75,21 +81,34 @@ function (
         imageParameters: _ip
     });
 
-    var _passages = new GraphicsLayer();
-    var _ships = new GraphicsLayer();
+    // Define passages layer
+    var _passages = new GraphicsLayer({
+        className: 'passages'
+    });
+    _passages.setRenderer(new SimpleRenderer(NORMAL_PASSAGE));
+
+    // Define ship layer
+    var _ships = new GraphicsLayer({
+        className: 'ships'
+    });
 
     // Add passages
     _passages.add(new Graphic(
         new Polyline(NORTH),
-        new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new Color([50, 50, 50, 0.5]), 10),
-        { name: 'north' }
+        null,
+        {
+            name: 'north',
+            selected: false
+        }
     ));
     _passages.add(new Graphic(
         new Polyline(SOUTH),
-        new SimpleLineSymbol(SimpleLineSymbol.STYLE_SHORTDASH, new Color([255, 0, 0, 1]), 10),
-        { name: 'south' }
+        null,
+        {
+            name: 'south',
+            selected: true
+        }
     ));
-    _passages.add(new Graphic(new Polyline(SOUTH)));
         
     // Add ships
     var _pic1 = new PictureMarkerSymbol('./img/ship1.png', 128, 128);
@@ -126,6 +145,9 @@ function (
         _passages,
         _ships
     ]);
+    _map.on('load', function () {
+        updatePassageSelection();
+    });
 
     _ships.on('mouse-down', function (e) {
         _map.disableMapNavigation();
@@ -142,10 +164,11 @@ function (
         ));
     });
     _ships.on('mouse-up', function (e) {
-        snapPoint(new Polyline(SOUTH), e.graphic);
+        var passage = getSelectedPassage();
+        snapPoint(passage, e.graphic);
         _ships.graphics.forEach(function (f) {
             if (f !== e.graphic) {
-                snapPointOpposite(new Polyline(SOUTH), e.graphic, f);
+                snapPointOpposite(passage, e.graphic, f);
             }
         });
         _map.enableMapNavigation();
@@ -154,10 +177,25 @@ function (
 
     // Snap ships
     _ships.graphics.forEach(function (e) {
-        snapPoint(new Polyline(SOUTH), e);
+        var passage = getSelectedPassage();
+        snapPoint(passage, e);
+    });
+
+    _passages.on('click', function (e) {
+        _passages.graphics.forEach(function (f) {
+            f.attributes.selected = (f === e.graphic);
+        });
+        _passages.remove(e.graphic);
+        _passages.add(e.graphic);
+        updatePassageSelection();
     });
   
     // Snap point  
+    function getSelectedPassage() {
+        return _passages.graphics.filter(function (f) {
+            return f.attributes.selected;
+        })[0].geometry;
+    }
     function snapPoint(polyline, graphic) {
         var nearest = geometryEngine.nearestCoordinate(polyline, graphic.geometry);
         graphic.setGeometry(nearest.coordinate);
@@ -216,5 +254,10 @@ function (
 
         // 
         target.setGeometry(opposite);
+    }
+    function updatePassageSelection() {
+        _passages.graphics.forEach(function (e) {
+            e.setSymbol(e.attributes.selected ? SELECTED_PASSAGE : null);
+        });
     }
 });
